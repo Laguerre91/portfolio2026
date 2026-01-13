f987cd0737f57cbc4fdf3c3de37a7e586ac45902
Lalo <aguerre.lara@gmail.com>
2026-01-13 20:42:43 +0100

    building footer and styling

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import './Footer.css';
+
+const Footer = () => {
+    return ( 
+        <section id="footer">
+            <div id='navigation-list'>  
+                <div id='left-container'>
+                    <a href="#home" className='footer-link'>Home</a>
+                    <a href="#about" className='footer-link'>About</a>
+                    <a href="#projects" className='footer-link'>Projects</a>
+                    <a href="#contact" className='footer-link'>Contact</a>
+                </div>
+
+                <div id='right-container'>
+                    <a href="https://www.linkedin.com/in/lara-aguerre-982b25221/" target="_blank" className='footer-link'>LinkedIn</a>
+                </div>
+            </div>
+            <p className='footer-title'>LARA AGUERRE</p>
+            <p className='footer-text'>Made with ❤️</p>
+        </section>
+    )
+}
+
+export default Footer;
