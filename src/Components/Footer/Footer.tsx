e4c821b61099bae5aa248ab70aa3957c68006861
Lalo <aguerre.lara@gmail.com>
2026-01-13 20:51:01 +0100

    styling footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,14 +4,17 @@ const Footer = () => {
     return ( 
         <section id="footer">
             <div id='navigation-list'>  
-                <div id='left-container'>
+                <div id='left-container' className='left-container'>
+                    <div className='navigate-title'>
+                        <p>NAVIGATE</p>
+                    </div>
                     <a href="#home" className='footer-link'>Home</a>
                     <a href="#about" className='footer-link'>About</a>
                     <a href="#projects" className='footer-link'>Projects</a>
                     <a href="#contact" className='footer-link'>Contact</a>
                 </div>
 
-                <div id='right-container'>
+                <div id='right-container' className='right-container'>
                     <a href="https://www.linkedin.com/in/lara-aguerre-982b25221/" target="_blank" className='footer-link'>LinkedIn</a>
                 </div>
             </div>
