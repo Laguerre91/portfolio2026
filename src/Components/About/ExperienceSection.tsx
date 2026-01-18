5f39e252fdee40ae53560a282f7b223fe22dd854
Lalo <aguerre.lara@gmail.com>
2026-01-18 12:02:18 +0100

    Animating experience title

--- src/Components/About/ExperienceSection.tsx
+++ src/Components/About/ExperienceSection.tsx
@@ -1,13 +1,24 @@
 import { Box, Heading } from '@chakra-ui/react';
+import { useRef } from 'react';
 import { EXPERIENCE } from '../../constants/experience';
 import ExperienceList from './ExperienceList';
 import styles from './ExperienceSection.module.css';
+import { useScrollPosition } from './useScrollPosition';
 
 const ExperienceSection = () => {
+    const titleRef = useRef<HTMLDivElement>(null);
+    const progress = useScrollPosition(titleRef, 0.5);
+
+    const translateY = -100 * (1 - progress); 
+    const opacity = progress;
+
   return (
     <Box as="section" id="experience" className={styles.experienceSection}>
       <Box className={styles.experienceContainer}>
-        <Heading className={styles.experienceTitle}>
+        <Heading ref={titleRef} className={styles.experienceTitle} style={{
+        transform: `translateY(${translateY}px)`,
+        opacity,
+      }}>
           Experience
         </Heading>
         
