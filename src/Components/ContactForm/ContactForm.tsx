ebd0966f83da9e742c1a83678fa05902b32c7f7d
Lalo <aguerre.lara@gmail.com>
2026-01-18 19:51:51 +0100

    adding context to footer

--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -1,7 +1,8 @@
 import { Box, Button, Input, Textarea, Field, Text } from "@chakra-ui/react";
 import { createToaster } from "@chakra-ui/react";
 import emailjs from "@emailjs/browser";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { useContact } from "../../context/ContactContext";
 import "./ContactForm.css";
 
 export const toaster = createToaster({
@@ -11,52 +12,54 @@ export const toaster = createToaster({
 const ContactForm = () => {
   const formRef = useRef<HTMLFormElement>(null);
 
+  const [confirmationMessage, setConfirmationMessage] = useState<string | null>(null);
+
+  const { closeContact } = useContact();
+
   const sendEmail = (e: React.FormEvent) => {
-    e.preventDefault();
+  e.preventDefault();
+  if (!formRef.current) return;
 
-    if (!formRef.current) return;
+  emailjs
+    .sendForm(
+      "service_2jsiljl",
+      "template_ah98s3e",
+      formRef.current,
+      "DcoIdTZT3xfOm5lCO"
+    )
+    .then(() => {
+      setConfirmationMessage("✅ Your message was sent successfully!");
+      formRef.current?.reset();
 
-    emailjs
-      .sendForm(
-        "service_2jsiljl",
-        "template_ah98s3e",
-        formRef.current,
-        "DcoIdTZT3xfOm5lCO"
-      )
-      .then(() => {
-        toaster.create({
-          title: "Message sent",
-          description: "Thank you for reaching out",
-          type: "success",
-        });
-        formRef.current?.reset();
-      })
-      .catch(() => {
-        toaster.create({
-          title: "Error",
-          description: "Something went wrong",
-          type: "error",
-        });
-      });
-  };
+      setTimeout(() => {
+        setConfirmationMessage(null);
+        closeContact();
+      }, 2000);
+    })
+    .catch(() => {
+      setConfirmationMessage("❌ Something went wrong, please try again.");
+    });
+};
+
 
   return (
     <Box className="contact-form-wrapper">
+    <Button className="close-btn" onClick={closeContact}>X</Button>
         <Text className="modal-title">Let's get in touch!</Text>
       <form ref={formRef} onSubmit={sendEmail}>
         <Field.Root required>
-          <Field.Label>Name</Field.Label>
-          <Input name="name" />
+          <Field.Label>Your Name</Field.Label>
+          <Input name="name" required/>
         </Field.Root>
 
         <Field.Root required>
-          <Field.Label>Email</Field.Label>
-          <Input name="email_id" type="email" />
+          <Field.Label>Your Email</Field.Label>
+          <Input name="email_id" type="email" required/>
         </Field.Root>
 
         <Field.Root required>
           <Field.Label>Message</Field.Label>
-          <Textarea name="message" rows={5} />
+          <Textarea name="message" rows={5} required/>
         </Field.Root>
 
           <input
@@ -64,6 +67,13 @@ const ContactForm = () => {
                 name="time"
                 value={new Date().toLocaleString()}
             />
+
+            {confirmationMessage && (
+                    <Text className="confirmation-message" mt={4}>
+                        {confirmationMessage}
+                    </Text>
+                    )}
+
 
         <Button type="submit" className="contact-button">
           Send
--- src/Components/Footer/FooterLinkList.tsx
+++ src/Components/Footer/FooterLinkList.tsx
@@ -1,7 +1,11 @@
 import { Box, Link } from '@chakra-ui/react';
 import type { LinkListProps } from '../../interfaces/interface';
+import { useContact } from '../../context/ContactContext';
 
 export const FooterLinkList = ({ title, links }: LinkListProps) => {
+
+  const { openContact } = useContact();
+
   return (
     <Box className="links-container">
       <Box className="navigate-title">
@@ -13,6 +17,12 @@ export const FooterLinkList = ({ title, links }: LinkListProps) => {
           key={link.label}
           href={link.href}
           className="footer-link"
+          onClick={(e) => {
+              if (link.label.toLowerCase() === 'contact') {
+                e.preventDefault();
+                openContact();
+              }
+            }}
           target={link.isExternal ? '_blank' : undefined}
         >
           {link.label}
